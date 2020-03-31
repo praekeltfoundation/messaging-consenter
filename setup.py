2f861025173fa67c540154dcb5bb9ceb5a1875bb
Kaitlyn Crawford <kaitlyn@praekelt.com>
2020-03-31 11:40:47 +0200

    Upgrade to Django 3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "Django>=2.2.2,<2.3",
+        "Django~=3.0",
         "django-environ>=0.4.5,<0.5",
         "rapidpro-python>=2.6.1,<2.7",
         "raven>=6.10.0,<7",
